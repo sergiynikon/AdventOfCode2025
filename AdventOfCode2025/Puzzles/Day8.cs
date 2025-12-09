7cb7fddfbfa0ba7be40ef14f4dcb85f38bd8de18
sergiynikon <sergiy.nikon2014@gmail.com>
2025-12-09 02:22:56 +0200

    Implement Day8

---
+++ AdventOfCode2025/Puzzles/Day8.cs
@@ -0,0 +1,196 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+
+namespace AdventOfCode2025.Puzzles;
+
+public class Day8 : IPuzzle
+{
+    private readonly struct Coordinate(long x, long y, long z) : IEquatable<Coordinate>
+    {
+        public long X { get; } = x;
+        public long Y { get; } = y;
+        public long Z { get; } = z;
+
+        public double DistanceTo(Coordinate other)
+        {
+            return Math.Sqrt((X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y) + (Z - other.Z) * (Z - other.Z));
+        }
+
+        public bool Equals(Coordinate other)
+        {
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Coordinate other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z);
+        }
+
+        public override string ToString()
+        {
+            return $"{X}, {Y}, {Z}";
+        }
+    }
+    public string[] Solve(string input)
+    {
+        return SolveInternal(input, 1000);
+    }
+
+    public string[] Solve(string input, int connectionsCount)
+    {
+        return SolveInternal(input, connectionsCount);
+    }
+
+    private string[] SolveInternal(string input, int connectionsCount)
+    {
+        var junctionBoxesCoordinates = ParseInput(input);
+
+        var part1Result = SolvePart1(junctionBoxesCoordinates, connectionsCount);
+        var part2Result = SolvePart2(junctionBoxesCoordinates);
+
+        return [part1Result.ToString(), part2Result.ToString()];
+    }
+
+    private long SolvePart1(Coordinate[] coordinates, int connectionsCount)
+    {
+        HashSet<(Coordinate c1, Coordinate c2, double distance)> pairs = [];
+
+        for (int i = 0; i < coordinates.Length - 1; i++)
+        {
+            for (int j = i + 1; j < coordinates.Length; j++)
+            {
+                double distance = coordinates[i].DistanceTo(coordinates[j]);
+                pairs.Add((coordinates[i], coordinates[j], distance));
+            }
+        }
+
+        var sortedPairs = pairs.OrderBy(p => p.distance).ToList();
+
+
+        int currentlyConnectedCount = 0;
+        HashSet<HashSet<Coordinate>> chains = new HashSet<HashSet<Coordinate>>();
+
+        int pairsIndex = 0;
+
+        while (currentlyConnectedCount < connectionsCount && pairsIndex < sortedPairs.Count)
+        {
+            var existingChain1 = chains.SingleOrDefault(chain => chain.Contains(sortedPairs[pairsIndex].c1));
+            var existingChain2 = chains.SingleOrDefault(chain => chain.Contains(sortedPairs[pairsIndex].c2));
+
+            if (existingChain1 != null && existingChain2 != null)
+            {
+                if (!Equals(existingChain1, existingChain2))
+                {
+                    // connect chains
+                    foreach (var coordinate in existingChain2)
+                    {
+                        existingChain1.Add(coordinate);
+                    }
+                    chains.Remove(existingChain2);
+                }
+            }
+            else if (existingChain1 != null && existingChain2 == null)
+            {
+                existingChain1.Add(sortedPairs[pairsIndex].c2);
+            }
+            else if (existingChain2 != null && existingChain1 == null)
+            {
+                existingChain2.Add(sortedPairs[pairsIndex].c1);
+            }
+            else
+            {
+                HashSet<Coordinate> newChain = [sortedPairs[pairsIndex].c1, sortedPairs[pairsIndex].c2];
+                chains.Add(newChain);
+            }
+
+            pairsIndex++;
+            currentlyConnectedCount++;
+        }
+
+        var product = chains.OrderByDescending(chain => chain.Count).Take(3).Aggregate(1, (current, chain) => current * chain.Count);
+
+        return product;
+    }
+    private long SolvePart2(Coordinate[] coordinates)
+    {
+        HashSet<(Coordinate c1, Coordinate c2, double distance)> pairs = [];
+
+        for (int i = 0; i < coordinates.Length - 1; i++)
+        {
+            for (int j = i + 1; j < coordinates.Length; j++)
+            {
+                double distance = coordinates[i].DistanceTo(coordinates[j]);
+                pairs.Add((coordinates[i], coordinates[j], distance));
+            }
+        }
+
+        var sortedPairs = pairs.OrderBy(p => p.distance).ToList();
+
+
+        int currentlyConnectedCount = 0;
+        HashSet<HashSet<Coordinate>> chains = new HashSet<HashSet<Coordinate>>();
+
+        int pairsIndex = 0;
+
+        (Coordinate c1, Coordinate c2, double distance) lastPair = sortedPairs[0];
+
+        while (chains.Count != 1 || chains.SelectMany(x => x).Count() != coordinates.Length)
+        {
+            var existingChain1 = chains.SingleOrDefault(chain => chain.Contains(sortedPairs[pairsIndex].c1));
+            var existingChain2 = chains.SingleOrDefault(chain => chain.Contains(sortedPairs[pairsIndex].c2));
+
+            if (existingChain1 != null && existingChain2 != null)
+            {
+                if (!Equals(existingChain1, existingChain2))
+                {
+                    // connect chains
+                    foreach (var coordinate in existingChain2)
+                    {
+                        existingChain1.Add(coordinate);
+                    }
+                    chains.Remove(existingChain2);
+                }
+            }
+            else if (existingChain1 != null && existingChain2 == null)
+            {
+                existingChain1.Add(sortedPairs[pairsIndex].c2);
+            }
+            else if (existingChain2 != null && existingChain1 == null)
+            {
+                existingChain2.Add(sortedPairs[pairsIndex].c1);
+            }
+            else
+            {
+                HashSet<Coordinate> newChain = [sortedPairs[pairsIndex].c1, sortedPairs[pairsIndex].c2];
+                chains.Add(newChain);
+            }
+
+            lastPair = sortedPairs[pairsIndex];
+
+            pairsIndex++;
+            currentlyConnectedCount++;
+        }
+
+        var result = lastPair.c1.X * lastPair.c2.X;
+
+        return result;
+    }
+
+    private Coordinate[] ParseInput(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        List<Coordinate> coordinates = new List<Coordinate>();
+        foreach (var line in lines)
+        {
+            var parts = line.Split(',');
+            coordinates.Add(new Coordinate(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2])));
+        }
+
+        return coordinates.ToArray();
+    }
+}
---
+++ UnitTests/Day8Tests.cs
@@ -0,0 +1,105 @@
+﻿using AdventOfCode2025.Puzzles;
+
+namespace UnitTests;
+
+public class Day8Tests
+{
+    private readonly Day8 _solver = new Day8();
+
+    [Fact]
+    public void Part1ExampleCase()
+    {
+        // Arrange
+        var input = """
+                    162,817,812
+                    57,618,57
+                    906,360,560
+                    592,479,940
+                    352,342,300
+                    466,668,158
+                    542,29,236
+                    431,825,988
+                    739,650,466
+                    52,470,668
+                    216,146,977
+                    819,987,18
+                    117,168,530
+                    805,96,715
+                    346,949,466
+                    970,615,88
+                    941,993,340
+                    862,61,35
+                    984,92,344
+                    425,690,689
+                    """;
+
+        // Act
+        var result = _solver.Solve(input, 10);
+
+        // Assert
+        var part1 = result[0];
+        Assert.Equal("40", part1);
+    }
+
+    [Theory]
+    [InlineData("""
+                1,1,1
+                2,2,2
+                """, 1, "2")]
+    [InlineData("""
+                1,1,1
+                2,2,2
+                4,4,4
+                """, 2, "3")]
+    [InlineData("""
+                1,1,1
+                2,2,2
+                4,4,4
+                8,8,8
+                9,9,9
+                """, 2, "4")]
+    public void Part1Cases(string input, int connectionsCount, string expectedResult)
+    {
+        // Act
+        var result = _solver.Solve(input, connectionsCount);
+
+        // Assert
+        var part1 = result[0];
+        Assert.Equal(expectedResult, part1);
+    }
+
+    [Fact]
+    public void Part2ExampleCase()
+    {
+        // Arrange
+        var input = """
+                    162,817,812
+                    57,618,57
+                    906,360,560
+                    592,479,940
+                    352,342,300
+                    466,668,158
+                    542,29,236
+                    431,825,988
+                    739,650,466
+                    52,470,668
+                    216,146,977
+                    819,987,18
+                    117,168,530
+                    805,96,715
+                    346,949,466
+                    970,615,88
+                    941,993,340
+                    862,61,35
+                    984,92,344
+                    425,690,689
+                    """;
+
+        // Act
+        var result = _solver.Solve(input);
+
+        // Assert
+        var part2 = result[1];
+        Assert.Equal("25272", part2);
+    }
+}
